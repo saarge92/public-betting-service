22c225bce10a94bc013c63faff7855cb3e8c7ce9
Serdar Durdyev <sedgarfetisovby@gmail.com>
2026-05-20 19:23:12 +0300

    Added validation

--- src/operation/user/dto.rs
+++ src/operation/user/dto.rs
@@ -1,8 +1,35 @@
-#[derive(Debug, Clone, PartialEq, serde::Deserialize)]
+use validator::Validate;
+
+#[derive(Debug, Clone, PartialEq, serde::Deserialize, Validate)]
 pub struct RegisterUserDto {
-    pub username: String,
+    #[validate(email(message = "Некорректный формат email"))]
     pub email: String,
+
+    #[validate(length(
+        min = 8,
+        max = 32,
+        message = "Пароль должен быть от 8 до 32 символов"
+    ))]
     pub password_raw: String,
+
+    #[validate(length(
+        min = 2,
+        message = "Имя пользователя должно содержать минимум 2 символа"
+    ))]
+    pub username: String,
+
+
+    #[validate(length(
+        min = 2,
+        max = 100,
+        message = "Имя должно содержать минимум 2 символа"
+    ))]
     pub first_name: Option<String>,
+
+    #[validate(length(
+        min = 2,
+        max = 100,
+        message = "Имя должно содержать минимум 2 символа"
+    ))]
     pub last_name: Option<String>,
 }
